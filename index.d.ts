405c45a31619882c72948b309053e477f963afa9
Daniel Smith <dannmasta@gmail.com>
2023-02-10 18:25:35 -0800

    improve typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,30 @@
-declare namespace config {
-
-    export function _merge (obj: object | string): object;
-    export function _defaults (obj: object | string): object;
-    export function _from (dir: string): object;
-
-    export namespace config {
-        export function _merge (obj: object | string): object;
-        export function _defaults (obj: object | string): object;
-        export function _from (dir: string): object;
-    }
-
+export interface ConfigDefaults {
+    enableArgv: boolean,
+    enableEnv: boolean,
+    setNodeEnv: boolean,
+    configDir: string,
+    configGroup: string,
+    config: string,
+    configId: string,
+    defaultConfigFileName: string
 }
 
-export = config;
+type Subset<T> = Partial<{
+    [P in keyof T]: T[P] extends object ? Subset<T[P]> : T[P]
+}>;
+
+export class Config {
+    constructor (opts?: Subset<ConfigDefaults>);
+    _opts: ConfigDefaults;
+    env (key: string, val?: any): any;
+    argv (key: string): string | undefined;
+    static deepFreeze (obj: object, cache: Map): object;
+    static isUndefined (val: any): boolean;
+    loadFromFile (file: string): object;
+    loadFromDir (dir: string): object;
+    load (): object;
+    resolve (): object;
+}
+
+declare const _config: Config;
+export default _config.resolve;
