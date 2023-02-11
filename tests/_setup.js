aedb373c6a0605bcdca8a389b4724fdb042495f0
Daniel Smith <dannmasta@gmail.com>
2023-02-10 18:23:12 -0800

    update tests setup

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,15 +1,17 @@
-// change process.cwd so we can load test default config
-process.chdir('./tests');
-
-// deps
-const chai = require('chai');
-const config = require('../index');
 const path = require('path');
+const chai = require('chai');
+
+process.env.CONFIG_DIR = path.join(__dirname, 'config');
+process.env.NODE_ENV = 'test';
+
+const Config = require('../lib/config');
+const config = require('../index');
 
 beforeEach(() => {
     global.assert = chai.assert;
     global.expect = chai.expect;
     global.should = chai.should();
+    global.Config = Config;
     global.config = config;
     global.path = path;
 });
