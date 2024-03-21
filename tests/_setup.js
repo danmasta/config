cf5d74bbf994e2e73b390f036663c8906fc417ec
Daniel Smith <dannmasta@gmail.com>
2024-03-20 22:39:00 -0700

    update tests setup

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const chai = require('chai');
 
 process.env.CONFIG_DIR = path.join(__dirname, 'config');
 process.env.NODE_ENV = 'test';
@@ -8,10 +7,12 @@ const Config = require('../lib/config');
 const config = require('../index');
 
 beforeEach(() => {
-    global.assert = chai.assert;
-    global.expect = chai.expect;
-    global.should = chai.should();
-    global.Config = Config;
-    global.config = config;
-    global.path = path;
+    return import('chai').then(chai => {
+        global.assert = chai.assert;
+        global.expect = chai.expect;
+        global.should = chai.should();
+        global.Config = Config;
+        global.config = config;
+        global.path = path;
+    });
 });
