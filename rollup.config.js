e3dbf79b2d6dc06d0c72ae6ec3da80064210a964
Daniel Smith <dannmasta@gmail.com>
2024-06-04 15:02:49 -0700

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve';
+
+export default [
+    {
+        input: [
+            'index.cjs',
+            'lib/config.js',
+            'lib/resolver.js'
+        ],
+        output: {
+            dir: 'dist/cjs',
+            format: 'cjs',
+            sourcemap: false,
+            strict: false,
+            preserveModules: true,
+            exports: 'auto',
+            entryFileNames: '[name].[format]',
+            esModule: false
+        },
+        plugins: [
+            resolve()
+        ],
+        external: [
+            '@danmasta/lo',
+            '@danmasta/lo/errors',
+            'minimist'
+        ]
+    }
+];
